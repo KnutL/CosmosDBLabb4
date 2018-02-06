253a3e046fafc00085e866437b5ee5dddf6ab622
chrilleson <Christoffer.Ljungqvist@hotmail.se>
2018-02-06 11:03:40 +0100

    Created user class

---
+++ CosmosDBLabb4/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+
+namespace CosmosDBLabb4
+{
+    class User
+    {
+        [JsonProperty(PropertyName ="id")]
+        public string email { get; set; }
+        public string bildUrl { get; set; }
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
